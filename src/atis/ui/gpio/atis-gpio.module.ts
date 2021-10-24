a85db807f7a2771118c2259570501272acaa9c30
Andrej Souček <andrej.soucek@betsys.com>
2021-10-24 11:08:04 +0200

    fix: provide gpio trigger

--- src/atis/ui/gpio/atis-gpio.module.ts
+++ src/atis/ui/gpio/atis-gpio.module.ts
@@ -1,8 +1,10 @@
 import { Logger, Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
+import { TriggerGpioService } from './trigger/trigger-gpio.service';
 
 @Module({
   providers: [
+    TriggerGpioService,
     {
       provide: 'GpioTriggerLogger',
       useFactory: () => new Logger('GpioTrigger'),
