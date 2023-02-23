dcc0d02293733f23b27c2f7bc458cb8fd6aa416a
Andrej Souček <andrej.soucek@betsys.com>
2023-02-23 19:37:45 +0100

    fix: set output gpio value to LOW on app start

--- src/atis/infrastructure/service/ptt/gpio-push-to-talk.service.ts
+++ src/atis/infrastructure/service/ptt/gpio-push-to-talk.service.ts
@@ -1,19 +1,32 @@
-import { Inject, Injectable, Logger, LoggerService, OnApplicationShutdown } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  Logger,
+  LoggerService,
+  OnApplicationBootstrap,
+  OnApplicationShutdown,
+} from '@nestjs/common';
 import { PushToTalkService } from '../../../domain/service/ptt/push-to-talk.service';
 import { Gpio } from 'onoff';
 import { OutputConfig } from '../../../domain/entity/config/output-config.entity';
+import {
+  GET_OUTPUT_CONFIG_REPOSITORY,
+  GetOutputConfigRepository,
+} from '../../../domain/repository/config/get-output-config.repository';
 
 @Injectable()
-export class GpioPushToTalkService implements PushToTalkService, OnApplicationShutdown {
+export class GpioPushToTalkService implements PushToTalkService, OnApplicationShutdown, OnApplicationBootstrap {
   private gpioOutput?: Gpio;
-  constructor(@Inject('InfrastructureLogger') private readonly logger: LoggerService) {
+  constructor(
+    @Inject('InfrastructureLogger') private readonly logger: LoggerService,
+    @Inject(GET_OUTPUT_CONFIG_REPOSITORY) private readonly cfgRepository: GetOutputConfigRepository,
+  ) {
     logger.debug('GPIO PTT service created');
   }
 
   startTransmission(config: OutputConfig): void {
-    this.gpioOutput = new Gpio(config.gpioOutputPin, 'out', 'none', { debounceTimeout: 10 });
-    this.gpioOutput.writeSync(Gpio.HIGH);
-    this.logger.debug(`PTT start: GPIO value: ${this.gpioOutput.readSync()}`);
+    this.gpioOutput?.writeSync(Gpio.HIGH);
+    this.logger.debug(`PTT start: GPIO value: ${this.gpioOutput?.readSync()}`);
   }
 
   stopTransmission(): void {
@@ -22,7 +35,13 @@ export class GpioPushToTalkService implements PushToTalkService, OnApplicationSh
   }
 
   onApplicationShutdown(signal?: string): void {
-    this.gpioOutput?.writeSync(Gpio.LOW);
-    this.gpioOutput?.unexport();
+    this.gpioOutput.writeSync(Gpio.LOW);
+    this.gpioOutput.unexport();
+  }
+
+  async onApplicationBootstrap(): Promise<void> {
+    const outputConfig = await this.cfgRepository.getOutputConfig();
+    this.gpioOutput = new Gpio(outputConfig.gpioOutputPin, 'out', 'none', { debounceTimeout: 10 });
+    this.gpioOutput.writeSync(Gpio.LOW);
   }
 }
